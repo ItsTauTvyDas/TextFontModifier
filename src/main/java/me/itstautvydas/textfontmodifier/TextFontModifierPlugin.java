3e7ae15ed46d1a295d919f5eb8af16dc22e58f33
Tautvydas <tautvydas.gaming@gmail.com>
2023-08-18 02:09:42 +0300

    Remake configuration and add config migration

--- src/main/java/me/itstautvydas/textfontmodifier/TextFontModifierPlugin.java
+++ src/main/java/me/itstautvydas/textfontmodifier/TextFontModifierPlugin.java
@@ -6,6 +6,8 @@ import com.comphenix.protocol.events.ListenerPriority;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 @Getter
 public final class TextFontModifierPlugin extends JavaPlugin {
 
@@ -13,6 +15,7 @@ public final class TextFontModifierPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        migrateConfiguration();
         setupConfiguration();
         textProcessor = new TextProcessor(this);
         ProtocolLibrary.getProtocolManager().addPacketListener(new TFMPacketListener(
@@ -23,15 +26,51 @@ public final class TextFontModifierPlugin extends JavaPlugin {
                 PacketType.Play.Server.SCOREBOARD_TEAM));
     }
 
+    private void noKeyFound(String ...keys) {
+        getLogger().warning("Configuration migration: Couldn't find configuration key(s) %s, skipping...".formatted(String.join(", ", keys)));
+    }
+
+    private void migrateConfiguration() {
+        getLogger().info("Checking for configuration migration...");
+        var config = getConfig();
+        if (config.getKeys(false).isEmpty()) {
+            getLogger().info("Empty configuration, creating...");
+            return;
+        }
+        if (config.isInt("config-version")) {
+            getLogger().info("Configuration key config-version exists, assuming there's no need for migration :)");
+            return;
+        }
+
+        config.set("config-version", 1);
+        if (config.isString("font") && config.isString("special-symbol-for-scoreboards")) {
+            config.set("fonts.my-custom-font.name", config.get("font"));
+            config.set("fonts.my-custom-font.special-symbol", config.get("special-symbol-for-scoreboards"));
+        } else noKeyFound("font", "special-symbol-for-scoreboards");
+        var regex = config.get("regex");
+        config.set("regex", null);
+        if (regex != null && config.isBoolean("invert-regex")) {
+            config.set("regex.value", regex);
+            config.set("regex.invert", config.get("invert-regex"));
+        } else noKeyFound("font", "invert-regex");
+        config.set("packets", null);
+        config.set("invert-regex", null);
+        config.set("font", null);
+        config.set("special-symbol-for-scoreboards", null);
+        saveConfig();
+    }
+
     private void setupConfiguration() {
-        getConfig().addDefault("font", "namespace:key");
-        getConfig().addDefault("regex", "[\\p{Print}&&[^~,],]+");
-        getConfig().addDefault("invert-regex", false);
-        getConfig().addDefault("packets.boss-bar", true);
-        getConfig().addDefault("packets.action-bar", true);
-        getConfig().addDefault("packets.scoreboard-title", true);
-        getConfig().addDefault("packets.scoreboard-scores", true);
-        getConfig().addDefault("special-symbol-for-scoreboards", "$u");
+        var config = getConfig();
+
+        config.addDefault("fonts.my-custom-font.name", "namespace:key");
+        config.addDefault("fonts.my-custom-font.special-symbol", "$u");
+        config.addDefault("regex.value", "[\\p{Print}&&[^~,],]+");
+        config.addDefault("regex.invert", false);
+        for (var key : List.of("boss-bar", "action-bar", "scoreboard-title", "scoreboard-scores")) {
+            config.addDefault("packets.%s.enable".formatted(key), true);
+            config.addDefault("packets.%s.forced-font".formatted(key), key.equals("scoreboard-scores") ? "my-custom-font" : "");
+        }
         getConfig().options().copyDefaults(true);
         saveConfig();
     }
